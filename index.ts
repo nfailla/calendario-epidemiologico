820c7ff5167b448312d85ab4609cdf1ed857fad6
nfailla <faillanicolas@hotmail.com>
2020-05-04 15:50:26 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -3,16 +3,24 @@ import './style.css';
 
 // Import MomentJS
 import moment from 'moment';
-import es from 'moment/locale/es';
+//import es from 'moment/locale/es';
+//moment().localeData('es', es);
 
-moment().localeData('es', es);
-const primerDia = moment('20191229', 'YYYYMMDD');
+//const primerDia = moment('20191229', 'YYYYMMDD');
+
+const primerDiaDelAnio = moment().startOf('year');
+console.log(
+  //primerDiaDelAnio.format('DD[/]MM[/]YYYY')
+  primerDiaDelAnio.startOf('week').format('DD[/]MM[/]YYYY')
+);
+
+const diaUno = primerDiaDelAnio.startOf('week');
 
 const appDiv: HTMLElement = document.getElementById('app');
 const input: HTMLElement = document.getElementById('input');
 const btn: HTMLElement = document.getElementById('btn');
 
-input.value = moment().format('DD[/]MM[/]YYYY');
+input['value'] = moment().format('DD[/]MM[/]YYYY');
 
 input.addEventListener('keyup', (event) => {
   if(event.keyCode === 13){
@@ -21,10 +29,10 @@ input.addEventListener('keyup', (event) => {
 });
 
 btn.addEventListener('click', () => {
-  const [DD, MM, YYYY] = input.value.split('/');
+  const [DD, MM, YYYY] = input['value'].split('/');
   const diaConsulta = moment(`${YYYY}${MM}${DD}`);
 
-  const result = calculateSemanaEpidemiologica(primerDia, diaConsulta);
+  const result = calculateSemanaEpidemiologica(diaUno, diaConsulta);
   appDiv.innerHTML = `<h4>${result}</h4>`;
 });
 
