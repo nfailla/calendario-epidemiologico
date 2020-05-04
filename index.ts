f2421c0e2b61a1bed22ed7c8a11339c9f6bbb7e1
nfailla <faillanicolas@hotmail.com>
2020-05-04 14:11:31 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,16 +1,24 @@
 // Import stylesheets
 import './style.css';
 
+// Import MomentJS
 import moment from 'moment';
 import es from 'moment/locale/es';
 
-// Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-
-let asd = 'holache';
-//let ahora = moment();
-//appDiv.innerHTML = `<h1>${moment}</h1>`;
 
 moment().localeData('es', es);
 
-console.log(moment().format('dddd'));
+//console.log(moment().format('dddd'));
+
+
+const primerDia = moment('20191229', 'YYYYMMDD');
+const hoy = moment();
+
+console.log('hoy: ' + hoy.format('dddd DD [de] MMMM [de] YYYY'));
+console.log('primerDia: ' + primerDia.format('dddd DD [de] MMMM [de] YYYY'));
+console.log('fromNow: ' + primerDia.fromNow());
+
+console.log('diferencia en días: ' + hoy.diff(primerDia, 'days'));
+
+
