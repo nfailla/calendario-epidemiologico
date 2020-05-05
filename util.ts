d3fbd999f5f092fb28c41500fbf8ebc0a7c5c0d7
nfailla <faillanicolas@hotmail.com>
2020-05-05 16:06:43 -0300

    1 file modified

--- util.ts
+++ util.ts
@@ -1,7 +1,10 @@
 import moment from 'moment';
 
 export function calculateSemanaEpidemiologica(DD, MM, YYYY): any[]{
-  if(YYYY==2021){ /* debug */ }
+  if(YYYY==2021){
+    /* debug */
+    1 + 1;
+  }
 
   //Valido que la fecha exista (p. ej. que no sea 29/02 en un ano no bisiesto, o una fecha inválida)
   if(!moment(`${YYYY}${MM}${DD}`)){
