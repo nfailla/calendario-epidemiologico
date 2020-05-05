382c071ba1369c06987ba1f99e68cf879afdabf9
nfailla <faillanicolas@hotmail.com>
2020-05-05 14:58:48 -0300

    1 file modified

--- calendario-tester.ts
+++ calendario-tester.ts
@@ -4,10 +4,26 @@ import * as util from './util';
 
 export function dibujar(YYYY){
 
-  let semanas = []; // Array de arrays
-  let anioAnterior = [];
-  let anioSiguiente = [];
+  let semanas = []; // Array de arrays (52 a 53 semanas)
+  let anioAnterior = []; //Array (una sola semana como máx)
+  let anioSiguiente = []; //Array (una sola semana como máx)
 
+  //Año anterior. La primera semana epidemiológica del año próximo puede absorber como máximo del 29 al 31 inclusive.
+  for(let dia = 29; dia>=31; dia++){
+    let semAnio: any[] = util.calculateSemanaEpidemiologica(dia, 12, YYYY); //DD, MM, YYYY
+    if(semAnio[1] == YYYY){ //Si el día pertenece a la primera semana epidemiológica del próximo año:
+      let nombreDia = traducir(moment(`${YYYY}${12}${dia}`).format('dddd'));
+
+      anioAnterior.push(`${nombreDia} ${dia}/${12}/${YYYY}`);
+      console.log(`anioAnterior, push: ${nombreDia} ${dia}/${12}/${YYYY}`);
+    }    
+  }
+  
+
+  //Año siguiente. La primera semana epidemiológica del año próximo puede absorber como máximo del 29 al 31 inclusive.
+
+
+  //Año de la consulta
   for(let mes = 1; mes <= 12; mes++){
     let MM = ('0' + mes).slice(-2); //Formato MM: 01, 02 ... 12
 
@@ -20,12 +36,12 @@ export function dibujar(YYYY){
         let nombreDia = traducir(moment(`${YYYY}${MM}${DD}`).format('dddd'));
 
         if (semAnio[1] < YYYY){
-          if (anioAnterior[semAnio[0]-1] == undefined){
-            anioAnterior[semAnio[0]-1] = [];
-          }
+          // if (anioAnterior[semAnio[0]-1] == undefined){
+          //   anioAnterior[semAnio[0]-1] = [];
+          // }
 
-          anioAnterior[semAnio[0]-1].push(`${nombreDia} ${DD}/${MM}/${YYYY}`);
-          console.log(`anioAnterior, push: ${nombreDia} ${DD}/${MM}/${YYYY}`);
+          // anioAnterior[semAnio[0]-1].push(`${nombreDia} ${DD}/${MM}/${YYYY}`);
+          // console.log(`anioAnterior, push: ${nombreDia} ${DD}/${MM}/${YYYY}`);
 
         } else if (semAnio[1] > YYYY){
           if (anioSiguiente[semAnio[0]-1] == undefined){
@@ -45,14 +61,10 @@ export function dibujar(YYYY){
 
   if(anioAnterior.length){
     console.log('Año anterior');
-    anioAnterior.forEach((sem, nroSemana) => {
-      let SE = `SE #${nroSemana + 1}:
+    anioAnterior.forEach(dia => {
+      let SE = `SE #1:
+      ${dia}
       `;
-      sem.forEach(dia => {
-        SE += `${dia}
-        `;
-      });
-
       console.log(SE);
     });
     console.log('================================================');
